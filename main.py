a9ffe32e2c7bf9fc9ca0757bb730e1e71c837e85
Peyman <hassani.peyman627@gmail.com>
2024-02-18 13:28:06 +0330

    Add complete button click

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
+import datetime
+import logging
 import random
+import sys
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+stdout_handler.setFormatter(formatter)
+
+logger.addHandler(stdout_handler)
+
 
 def main():
     url = 'https://nobat.mex.co.ir/'
     browser = webdriver.Chrome()
+
     browser.get(url)
 
-    wait = WebDriverWait(browser, 10)
+    start_time = datetime.time(16, 0, 0)
+    while True:
+        time.sleep(0.1)
+
+        if datetime.datetime.now().time() >= start_time:
+            break
+
+    wait = WebDriverWait(browser, 300)
 
     wait.until(EC.element_to_be_clickable(
         (By.XPATH, '//*[@id="__next"]/div[1]/div[2]/div[2]/div[2]/div/span/button')
@@ -39,6 +62,10 @@ def main():
         By.XPATH, '//*[@id="__next"]/div[1]/div[2]/div[2]/div[2]/div/div[3]/span[2]/button'
     )
     complete_button.click()
+
+    time.sleep(600)
+
+    browser.quit()
 
 
 if __name__ == '__main__':
